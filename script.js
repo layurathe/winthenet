5b8b715c48657fb8a6692cf3b1fe82e1131fad08
layurathe <laura.li_li@outlook.com>
2025-03-20 03:48:01 -0400

    script
    added popup script (not working)

---
+++ script.js
@@ -0,0 +1,38 @@
+var texts = [
+    "Doc Laura Li",
+    "Link Below",
+    "Buy Now"
+];
+
+var currentIndex = 0;
+
+function accept() {
+    window.close();
+    window.location.href = 'Page2.html';
+}
+
+function displayTextInOrder() {
+    if (currentIndex < texts.length) {
+        var textBox = document.createElement("div");
+        textBox.className = "textBox"; 
+        textBox.textContent = texts[currentIndex];
+
+        var xPos = Math.floor(Math.random() * (window.innerWidth - 200)); 
+        var yPos = Math.floor(Math.random() * (window.innerHeight - 100)); 
+        textBox.style.position = "absolute";
+        textBox.style.left = xPos + "px";
+        textBox.style.top = yPos + "px";
+
+        document.body.appendChild(textBox);
+
+        currentIndex++;
+
+        var randomInterval = Math.floor(Math.random() * (30000 - 5000 + 1)) + 5000;
+
+        setTimeout(displayTextInOrder, randomInterval);
+    }
+}
+
+setTimeout(function() {
+    displayTextInOrder();
+}, 20000);
